a99f2bd441e129b321cd729e114a202501a339b5
German1127 <kleverk2023@gmail.com>
2024-04-12 11:41:13 -0700

    fixed task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,48 +2,49 @@
 """Flask instance"""
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 """Flask instance"""
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
-def hello_route():
+
+@app.route('/')
+def display_root():
     """returns a simple string"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hello_route_v1():
+@app.route('/hbnb')
+def display_hbnb():
     """returns HBNB string"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def hello_route_v2(text):
+def display_c(text):
     """Function that returns a C string"""
-    return 'C ' + text.replace('_', ' ')
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def hello_route_v3(text='is cool'):
+@app.route('/python/')
+@app.route('/python/<text>')
+def display_python(text="is cool"):
     """Function that returns a Python string"""
-    return 'Python ' + text.replace('_', ' ')
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def hello_route_v4(n):
+@app.route('/number/<int:n>')
+def display_n(n):
     """returns a string only if n is a number"""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def hello_route_v5(n):
-    """Returns a template with the given number."""
-    return render_template('5-number.html', number=n)
+@app.route('/number_template/<int:n>')
+def display_n_template(n):
+    """Return an html page with value of n"""
+    return render_template('5-number.html', n=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
