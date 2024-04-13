b8b9b3cba6fd2731d33a2827dfab2e131392606a
German1127 <kleverk2023@gmail.com>
2024-04-13 06:45:27 -0700

    fix

--- models/state.py
+++ models/state.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""state class."""
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+import models
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+""" `getenv` to determine in which storage type we are
+by scanning the HBNB_TYPE_STORAGE"""
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database"""
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    """
+    State class
+    Establish a relationship with the class City
+    """
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        """ db ==>  means let's go for SQLAlchemy logic"""
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """
+        Getter - returns the list of City instances
+        with state_id == State.id
+        FileStorage relationship between State and City
+        """
+        l_cities = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                l_cities.append(city)
+        return l_cities
